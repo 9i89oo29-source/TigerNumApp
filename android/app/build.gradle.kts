af7d9aa7b9b3030bf9124d78af2490bc10f61f02
9i89oo29-source <9i89oo29@gmail.com>
2026-05-17 17:08:06 +0000

    Fix Hilt missing types error by enabling correctErrorTypes in KSP

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -142,3 +142,7 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
+
+ksp {
+    arg("correctErrorTypes", "true")
+}
