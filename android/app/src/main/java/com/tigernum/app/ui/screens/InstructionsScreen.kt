598b9ddfc198a0f71dc475d8aaf399d9c57963e3
9i89oo29-source <9i89oo29@gmail.com>
2026-05-18 01:42:30 +0300

    Update InstructionsScreen.kt

--- android/app/src/main/java/com/tigernum/app/ui/screens/InstructionsScreen.kt
+++ android/app/src/main/java/com/tigernum/app/ui/screens/InstructionsScreen.kt
@@ -3,6 +3,7 @@ package com.tigernum.app.ui.screens
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme   // 👈 إضافة هذا الاستيراد
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
