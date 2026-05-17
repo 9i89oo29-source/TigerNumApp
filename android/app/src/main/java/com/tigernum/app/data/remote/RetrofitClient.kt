6bc77fcce625fc7f6d2cd8c20306af37174a0c99
9i89oo29-source <9i89oo29@gmail.com>
2026-05-18 01:52:22 +0300

    Update RetrofitClient.kt

--- android/app/src/main/java/com/tigernum/app/data/remote/RetrofitClient.kt
+++ android/app/src/main/java/com/tigernum/app/data/remote/RetrofitClient.kt
@@ -1,5 +1,6 @@
 package com.tigernum.app.data.remote
 
+import com.tigernum.app.BuildConfig
 import com.google.gson.GsonBuilder
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
